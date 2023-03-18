d323f5947e81a29246d6c5797c71594d35cd244b
NKDuy <kn145660@gmail.com>
2023-03-18 19:52:12 +0700

    chore: GamePanel class, JPanel

---
+++ src/com/nkduy/platformer/GamePanel.java
@@ -0,0 +1,15 @@
+package com.nkduy.platformer;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+
+    public GamePanel() {
+        //
+    }
+
+    public void paintComponents(Graphics g) {
+        //
+    }
+}
