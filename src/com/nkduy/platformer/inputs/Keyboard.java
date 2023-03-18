615e2d612e9908def559ffe1424029fabd98a895
NKDuy <kn145660@gmail.com>
2023-03-18 20:20:21 +0700

    chore: Checking for specific key inputs

--- src/com/nkduy/platformer/inputs/Keyboard.java
+++ src/com/nkduy/platformer/inputs/Keyboard.java
@@ -10,11 +10,22 @@ public class Keyboard implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_W:
+                System.out.println("Its W");
+                break;
+            case KeyEvent.VK_A:
+                System.out.println("Its A");
+                break;
+            case KeyEvent.VK_S:
+                System.out.println("Its S");
+                break;
+            case KeyEvent.VK_D:
+                System.out.println("Its D");
+                break;
+        }
     }
 
     @Override
-    public void keyReleased(KeyEvent e) {
-        System.out.println("A key is pressed");
-    }
+    public void keyReleased(KeyEvent e) { }
 }
