67db98e3619da9f141322d65e926401238a6f38e
NKDuy <kn145660@gmail.com>
2023-03-18 20:12:22 +0700

    chore: Adding KeyboardInputs

--- src/com/nkduy/platformer/GamePanel.java
+++ src/com/nkduy/platformer/GamePanel.java
@@ -2,11 +2,26 @@ package com.nkduy.platformer;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class GamePanel extends JPanel {
 
     public GamePanel() {
-        //
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) { }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
     }
 
     public void paintComponents(Graphics g) {
---
+++ src/com/nkduy/platformer/inputs/Keyboard.java
@@ -0,0 +1,20 @@
+package com.nkduy.platformer.inputs;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Keyboard implements KeyListener {
+
+    @Override
+    public void keyTyped(KeyEvent e) { }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
