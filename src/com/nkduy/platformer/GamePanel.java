46da643cbd8ab482806b2a8ed7317f4a74f146a9
NKDuy <kn145660@gmail.com>
2023-03-20 17:26:23 +0700

    chore: cleanUp

--- src/com/nkduy/platformer/GamePanel.java
+++ src/com/nkduy/platformer/GamePanel.java
@@ -5,21 +5,13 @@ import com.nkduy.platformer.inputs.Mouse;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.util.ArrayList;
-import java.util.Random;
 
 public class GamePanel extends JPanel {
 
     Mouse mouse;
     float xDelta = 100, yDelta = 100;
-    float xDir = 1f, yDir = 1f;
-    Color color = new Color(150, 20, 90);
-    Random random;
 
     public GamePanel() {
-        random = new Random();
         mouse = new Mouse(this);
 
         setPanelSize();
@@ -29,7 +21,7 @@ public class GamePanel extends JPanel {
     }
 
     private void setPanelSize() {
-        Dimension size = new Dimension(960, 480); // w: 960/32 = 30 imgs; h: 480/32 = 15 imgs
+        Dimension size = new Dimension(960, 480); // w: 960/32 = 30 images; h: 480/32 = 15 images
         setMinimumSize(size);
         setPreferredSize(size);
         setMaximumSize(size);
@@ -50,31 +42,5 @@ public class GamePanel extends JPanel {
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
-        updateRectangle();
-        g.setColor(color);
-        g.fillRect((int) xDelta, (int) yDelta, 200, 50);
-    }
-
-    private void updateRectangle() {
-        xDelta += xDir;
-        if (xDelta > 400 || xDelta < 0) {
-            xDir *= -1;
-            color = getRandColor();
-        }
-
-        yDelta += yDir;
-        if (yDelta > 400 || yDelta < 0) {
-            yDir *= -1;
-            color = getRandColor();
-        }
-    }
-
-    private Color getRandColor() {
-        int r = random.nextInt(255);
-        int g = random.nextInt(255);
-        int b = random.nextInt(255);
-
-        return new Color(r, g, b);
     }
 }
