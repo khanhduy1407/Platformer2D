50b8b5cbe71368bd3139132630c0aecacf4282b0
NKDuy <kn145660@gmail.com>
2023-03-20 20:37:03 +0700

    chore: FPS vs UPS

--- src/com/nkduy/platformer/Game.java
+++ src/com/nkduy/platformer/Game.java
@@ -8,6 +8,7 @@ public class Game implements Runnable {
     Thread gameThread;
 
     final int FPS_SET = 120;
+    final int UPS_SET = 200;
 
     public Game() {
         gamePanel = new GamePanel();
@@ -26,14 +27,33 @@ public class Game implements Runnable {
     @Override
     public void run() {
         double timePerFrame = 1000000000.0 / FPS_SET;
+        double timePerUpdate = 1000000000.0 / UPS_SET;
         long lastFrame = System.nanoTime();
         long now = System.nanoTime();
 
+        long previousTime = System.nanoTime();
+
         int frames = 0;
+        int updates = 0;
         long lastCheck = System.currentTimeMillis();
+
+        double deltaU = 0;
 
         while (true) {
             now = System.nanoTime();
+            long currentTime = System.nanoTime();
+
+            // deltaU will be 1.0 or more WHEN the duration since last update is equal
+            // OR more than timePerUpdate
+            deltaU += (currentTime - previousTime) / timePerUpdate;
+            previousTime = currentTime;
+
+            if (deltaU > 1) {
+                // update();
+                updates++;
+                deltaU--;
+            }
+
             if (now - lastFrame >= timePerFrame) {
                 gamePanel.repaint();
                 lastFrame = now;
@@ -44,8 +64,9 @@ public class Game implements Runnable {
             // Save the newFps check as the lastFps check and repeat.
             if (System.currentTimeMillis() - lastCheck >= 1000) {
                 lastCheck = System.currentTimeMillis();
-                System.out.println("FPS: " + frames);
+                System.out.println("FPS: " + frames + " | UPS: " + updates);
                 frames = 0;
+                updates = 0;
             }
         }
     }
