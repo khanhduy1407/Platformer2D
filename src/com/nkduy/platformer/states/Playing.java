d019ea8e3d391ede01555c0f1a7d05db48690533
NKDuy <kn145660@gmail.com>
2023-03-26 17:07:46 +0700

    chore: SoundButton class

--- src/com/nkduy/platformer/states/Playing.java
+++ src/com/nkduy/platformer/states/Playing.java
@@ -14,7 +14,7 @@ public class Playing extends State implements StateMethods {
     Player player;
     LevelManager levelManager;
     PauseOverlay pauseOverlay;
-    boolean paused;
+    boolean paused = true;
 
     public Playing(Game game) {
         super(game);
@@ -32,6 +32,8 @@ public class Playing extends State implements StateMethods {
     public void update() {
         levelManager.update();
         player.update();
+
+        pauseOverlay.update();
     }
 
     @Override
@@ -51,17 +53,23 @@ public class Playing extends State implements StateMethods {
 
     @Override
     public void mousePressed(MouseEvent e) {
-
+        if (paused) {
+            pauseOverlay.mousePressed(e);
+        }
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-
+        if (paused) {
+            pauseOverlay.mouseReleased(e);
+        }
     }
 
     @Override
     public void mouseMoved(MouseEvent e) {
-
+        if (paused) {
+            pauseOverlay.mouseMoved(e);
+        }
     }
 
     @Override
