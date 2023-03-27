e5459fe5a99df8fb3eb26d1f32434728680eee3b
NKDuy <kn145660@gmail.com>
2023-03-27 19:44:32 +0700

    fix: Fixing slider image y

--- src/com/nkduy/platformer/ui/VolumeButton.java
+++ src/com/nkduy/platformer/ui/VolumeButton.java
@@ -30,7 +30,7 @@ public class VolumeButton extends PauseButton {
         for (int i = 0; i < imgs.length; i++) {
             imgs[i] = temp.getSubimage(i*VOLUME_DEFAULT_WIDTH, 0, VOLUME_DEFAULT_WIDTH, VOLUME_DEFAULT_HEIGHT);
         }
-        slider = temp.getSubimage(3*VOLUME_DEFAULT_WIDTH, y, SLIDER_DEFAULT_WIDTH, VOLUME_DEFAULT_HEIGHT);
+        slider = temp.getSubimage(3*VOLUME_DEFAULT_WIDTH, 0, SLIDER_DEFAULT_WIDTH, VOLUME_DEFAULT_HEIGHT);
     }
 
     public void update() {
