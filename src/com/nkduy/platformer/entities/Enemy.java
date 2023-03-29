50c65b3a025390b700153a469403ea00d96d52ce
NKDuy <kn145660@gmail.com>
2023-03-29 10:33:03 +0700

    chore: Enemy falling

--- src/com/nkduy/platformer/entities/Enemy.java
+++ src/com/nkduy/platformer/entities/Enemy.java
@@ -1,11 +1,18 @@
 package com.nkduy.platformer.entities;
 
+import com.nkduy.platformer.main.Game;
+
 import static com.nkduy.platformer.util.Constants.EnemyConstants.*;
+import static com.nkduy.platformer.util.HelpMethods.*;
 
 public abstract class Enemy extends Entity {
 
     int animIndex, enemyState, enemyType;
     int animTick, animSpeed = 25;
+    boolean firstUpdate = true;
+    boolean inAir;
+    float fallSpeed;
+    float gravity = 0.04f * Game.SCALE;
 
     public Enemy(float x, float y, int width, int height, int enemyType) {
         super(x, y, width, height);
@@ -31,7 +38,22 @@ public abstract class Enemy extends Entity {
     }
 
     private void updateMove(int[][] lvlData) {
-        //
+        if (firstUpdate) {
+            if (!IsEntityOnFloor(hitbox, lvlData)) {
+                inAir = true;
+            }
+        }
+        if (inAir) {
+            if (CanMoveHere(hitbox.x, hitbox.y, hitbox.width, hitbox.height, lvlData)) {
+                hitbox.y += fallSpeed;
+                fallSpeed += gravity;
+            } else {
+                inAir = false;
+                hitbox.y = GetEntityYPosUnderRoofOrAboveFloor(hitbox, fallSpeed);
+            }
+        } else {
+            //
+        }
     }
 
     public int getAnimIndex() {
