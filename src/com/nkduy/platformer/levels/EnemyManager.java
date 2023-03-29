bc3bbf630392440171db065e96dae1e3cf1a2aa2
NKDuy <kn145660@gmail.com>
2023-03-29 10:02:37 +0700

    chore: Enemy Manager

---
+++ src/com/nkduy/platformer/levels/EnemyManager.java
@@ -0,0 +1,50 @@
+package com.nkduy.platformer.levels;
+
+import com.nkduy.platformer.entities.Crabby;
+import com.nkduy.platformer.states.Playing;
+import com.nkduy.platformer.util.LoadSave;
+
+import static com.nkduy.platformer.util.Constants.EnemyConstants.*;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+public class EnemyManager {
+
+    Playing playing;
+    BufferedImage[][] crabbyArr;
+    ArrayList<Crabby> crabbies = new ArrayList<>();
+
+    public EnemyManager(Playing playing) {
+        this.playing = playing;
+
+        loadEnemyImgs();
+    }
+
+    public void update() {
+        for (Crabby c: crabbies) {
+            c.update();
+        }
+    }
+
+    public void draw(Graphics g) {
+        drawCrabs(g);
+    }
+
+    private void drawCrabs(Graphics g) {
+        for (Crabby c: crabbies) {
+            g.drawImage(crabbyArr[c.getEnemyState()][c.getAnimIndex()], (int) c.getHitbox().x, (int) c.getHitbox().y, CRABBY_WIDTH, CRABBY_HEIGHT, null);
+        }
+    }
+
+    private void loadEnemyImgs() {
+        crabbyArr = new BufferedImage[5][9];
+        BufferedImage temp = LoadSave.GetSpriteAtlas(LoadSave.CRABBY_SPRITE);
+        for (int j = 0; j < crabbyArr.length; j++) {
+            for (int i = 0; i < crabbyArr[j].length; i++) {
+                crabbyArr[j][i] = temp.getSubimage(i*CRABBY_WIDTH_DEFAULT, j*CRABBY_HEIGHT_DEFAULT, CRABBY_WIDTH_DEFAULT, CRABBY_HEIGHT_DEFAULT);
+            }
+        }
+    }
+}
