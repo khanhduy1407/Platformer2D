9ccc7ada4b7a7a92d42f16eb0c86986db201a72e
NKDuy <kn145660@gmail.com>
2023-03-29 10:22:31 +0700

    fix: Crabby hitbox

--- src/com/nkduy/platformer/entities/Crabby.java
+++ src/com/nkduy/platformer/entities/Crabby.java
@@ -1,10 +1,14 @@
 package com.nkduy.platformer.entities;
 
+import com.nkduy.platformer.main.Game;
+
 import static com.nkduy.platformer.util.Constants.EnemyConstants.*;
 
 public class Crabby extends Enemy {
 
     public Crabby(float x, float y) {
         super(x, y, CRABBY_WIDTH, CRABBY_HEIGHT, CRABBY);
+
+        initHitbox(x, y, (int) (22* Game.SCALE), (int) (19*Game.SCALE));
     }
 }
